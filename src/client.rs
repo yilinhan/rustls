8bfc6b48f68306ef8e24a007242469a56eed939f
Joseph Birr-Pixton <jpixton@gmail.com>
2016-11-06 20:43:20 +0000

    Allow ticket receipt during traffic stage
    (plus remove old debug)

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,4 @@
-use msgs::enums::{CipherSuite, ProtocolVersion};
+use msgs::enums::CipherSuite;
 use msgs::enums::{AlertDescription, HandshakeType, ExtensionType};
 use session::{Session, SessionSecrets, SessionRandoms, SessionCommon};
 use suites::{SupportedCipherSuite, ALL_CIPHERSUITES};
@@ -304,7 +304,8 @@ pub enum ConnState {
   ExpectNewTicketResume,
   ExpectCCSResume,
   ExpectFinishedResume,
-  Traffic
+  TrafficTLS12,
+  TrafficTLS13
 }
 
 pub struct ClientSessionImpl {
@@ -381,7 +382,10 @@ impl ClientSessionImpl {
   }
 
   pub fn is_handshaking(&self) -> bool {
-    self.state != ConnState::Traffic
+    match self.state {
+      ConnState::TrafficTLS12 | ConnState::TrafficTLS13 => true,
+      _ => false
+    }
   }
 
   pub fn process_msg(&mut self, mut msg: Message) -> Result<(), TLSError> {
@@ -390,8 +394,6 @@ impl ClientSessionImpl {
       let dm = try!(self.common.decrypt_incoming(msg));
       msg = dm;
     }
-
-    println!("incoming {:?}", msg);
 
     /* For handshake messages, we need to join them before parsing
      * and processing. */
@@ -438,7 +440,7 @@ impl ClientSessionImpl {
   fn process_hello_req(&mut self) {
     /* If we're post handshake, send a refusal alert.
      * Otherwise, drop it silently. */
-    if self.state == ConnState::Traffic {
+    if !self.is_handshaking() {
       self.common.send_warning_alert(AlertDescription::NoRenegotiation);
     }
   }
@@ -458,7 +460,7 @@ impl ClientSessionImpl {
     self.state = new_state;
 
     /* Once we're connected, start flushing sendable_plaintext. */
-    if self.state == ConnState::Traffic && !self.common.traffic {
+    if !self.is_handshaking() && !self.common.traffic {
       self.common.start_traffic();
     }
 
@@ -480,7 +482,8 @@ impl ClientSessionImpl {
       ConnState::ExpectNewTicketResume => &client_hs::EXPECT_NEW_TICKET_RESUME,
       ConnState::ExpectCCSResume => &client_hs::EXPECT_CCS_RESUME,
       ConnState::ExpectFinishedResume => &client_hs::EXPECT_FINISHED_RESUME,
-      ConnState::Traffic => &client_hs::TRAFFIC
+      ConnState::TrafficTLS12 => &client_hs::TRAFFIC_TLS12,
+      ConnState::TrafficTLS13 => &client_hs::TRAFFIC_TLS13,
     }
   }
 
